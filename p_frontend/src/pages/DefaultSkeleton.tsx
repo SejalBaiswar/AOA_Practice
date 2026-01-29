8f0e6219a24795e6cb357347d09105bed34fe4cf
Abhay8878 <abhayraghuwanshi877@gmail.com>
2026-01-29 16:06:19 +0530

    Skeleton Fixed

--- p_frontend/src/pages/DefaultSkeleton.tsx
+++ p_frontend/src/pages/DefaultSkeleton.tsx
@@ -5,8 +5,8 @@ export default function DefaultSkeleton() {
     <div className="flex items-center gap-4">
       <Skeleton className="h-12 w-12 rounded-full" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[1200px]" />
+        <Skeleton className="h-4 w-[1100px]" />
       </div>
     </div>
   );
